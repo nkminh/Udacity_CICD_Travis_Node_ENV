1a29cfe5d3467e61954c93354b7c78e8517e8e79
minh-nguyen <nguyenkhanhminh1993@gmail.com>
2022-08-09 00:46:26 +0700

    add read env in main

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
+    const favoriteFood = process.env.FAVORITE_FOOD;
+    console.log(`My favorite food is ${favoriteFood}`);
     console.log("Microservices rock!");
     await sleep(5000);
   }
